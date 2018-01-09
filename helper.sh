aeb34cb5bc4b195205e544fb985aff40fb4c5f75
Tae Geun, Kim <edeftg@gmail.com>
2018-01-09 16:32:22 +0900

    Finish Jupyter

--- helper.sh
+++ helper.sh
@@ -2,4 +2,4 @@ echo "# HEP Docker" >> ~/.bashrc
 echo 'alias inithepframe="docker run -it --name hepframe --net=host axect/hepframework:0.1 /bin/bash"' >> ~/.bashrc
 echo 'alias hepframe="docker start hepframe && docker attach hepframe"' >> ~/.bashrc
 
-
+docker cp ./jupyter.sh hepframe:/opt/
--- jupyter.sh
+++ jupyter.sh
@@ -1,9 +1,2 @@
-echo "# Jupyter" >> ~/.bashrc
-export DIRECTORY="$HOME/zlib/notebooks"
-if [ ! -d "${DIRECTORY}" ]; 
-then
-	mkdir -p $HOME/zlib/notebooks
-fi
-echo 'export NOTEBOOK="$HOME/zlib/notebooks"' >> ~/.bashrc
-echo 'alias initjupyter="docker run --name hepframejupyter -it -p 8888:8888 -v ${NOTEBOOK}:/opt/notebooks axect/hepframework:0.1"' >> ~/.bashrc
-echo 'alias hepjupyter="docker start hepframejupyter"' >> ~/.bashrc
+jupyter-notebook --no-browser --port=8888 --allow-root
+
